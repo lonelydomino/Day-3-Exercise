0bcc55c7ab03dcae3dc0b4cee6b48db7a72f04c0
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-06-07 16:22:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+if number
+#Write your code below this line 👇
+
+
+
+
